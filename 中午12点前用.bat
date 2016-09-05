220e5fe4e9cb6f4f31e23646204d284e96e8ebf1
CR23-Yzlong <97507483@qq.com>
2016-09-05 13:52:59 +0800

    9.5Start

---
+++ 中午12点前用.bat
@@ -0,0 +1,5 @@
+
+set da=%date:~8,2%
+set /a day=%da%-1
+
+ren *.rar  CR23_Ò¶ÕêÁú_%date:~0,4%.%date:~5,2%.%day%.rar
---
+++ 中午12点后用.bat
@@ -0,0 +1,2 @@
+@echo off
+ren *.rar CR23_Ò¶ÕêÁú_%date:~0,4%.%date:~5,2%.%date:~8,2%.rar
